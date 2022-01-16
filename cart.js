89d8eab09426eaabbe941bfa7de5b2c7571ee7b1
iMrihan <mhd.khan.rihan@gmail.com>
2022-01-16 23:57:04 +0530

    some changes

--- cart.js
+++ cart.js
@@ -1,26 +1,34 @@
 /** @format */
 
 let cartArr = JSON.parse(localStorage.getItem("cartData")) || [];
-console.log(cartArr);
+
+// console.log(cartArr, priceArr);
 let append_div = document.querySelector(".container");
 let items = document.getElementById("total-items");
+let totalCost = document.getElementById("total-cost");
 
 cartData(cartArr);
-console.log(items);
+// console.log(items, priceArr);
 
 function cartData(cartArr) {
+	var sum = 0;
 	console.log(cartArr.length);
 
 	append_div.textContent = "";
 	items.innerHTML = cartArr.length;
 
+	for (var i = 0; i < cartArr.length; i++) {
+		sum += Number(cartArr[i].price);
+	}
+	console.log(sum);
+	totalCost.innerHTML = sum;
+
 	cartArr.map(function (elem, index) {
-		let prices = Math.floor(Math.random() * (500 - 100) + 100);
 		let div = document.createElement("div");
 		let image = document.createElement("img");
 		image.src = elem.strCategoryThumb;
 		let price = document.createElement("p");
-		price.textContent = `price: ${prices}`;
+		price.textContent = `price: ${elem.price}`;
 		let btn = document.createElement("button");
 		btn.textContent = "Delete";
 		btn.addEventListener("click", function () {
@@ -34,5 +42,6 @@ function cartData(cartArr) {
 function deleteTask(index) {
 	cartArr.splice(index, 1);
 	localStorage.setItem("cartData", JSON.stringify(cartArr));
+
 	cartData(cartArr);
 }
--- menu.js
+++ menu.js
@@ -1,57 +1,49 @@
+/** @format */
 
-    let cartData = JSON.parse(localStorage.getItem("cartData"))||[];
+let cartData = JSON.parse(localStorage.getItem("cartData")) || [];
 
+async function fetchApi() {
+	try {
+		let res = await fetch(
+			"https://www.themealdb.com/api/json/v1/1/categories.php"
+		);
+		let data = await res.json();
 
+		let response = data.categories;
 
+		console.log("data:", response);
+		disses(response);
+	} catch (err) {
+		console.log("err:", err);
+	}
+}
+fetchApi();
+let append_div = document.querySelector(".container");
 
-async function fetchApi(){
- 
- try{
-  let res = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
-  let data = await res.json();
-  
-  let response = data.categories;
- 
-  console.log("data:", response);
-  disses(response);
+function disses(data) {
+	data.map(function (elem) {
+		let prices = Math.floor(Math.random() * 500 + 100);
+		let div = document.createElement("div");
+		let image = document.createElement("img");
+		image.src = elem.strCategoryThumb;
+		let price = document.createElement("p");
+		price.textContent = `price: ${prices}`;
+		let btn = document.createElement("button");
+		btn.textContent = "Add to cart";
+		btn.addEventListener("click", function () {
+			elem.price = prices;
+			console.log("elem:", elem);
+			addItems(elem);
+		});
 
- }
- catch(err){
-     console.log("err:",err);
- }
+		div.append(image, price, btn);
+		append_div.append(div);
+	});
+}
 
+function addItems(elem) {
+	cartData.push(elem);
 
- }
- fetchApi();
- let append_div = document.querySelector(".container");
-
- function disses(data){
-     
-   data.map(function(elem){
-    let prices = Math.floor(Math.random() * 500 +100);
-    let div = document.createElement("div");
-    let image = document.createElement("img");
-    image.src = elem.strCategoryThumb;
-    let price = document.createElement("p")
-    price.textContent = `price: ${prices}`;
-    let btn = document.createElement("button");
-    btn.textContent = "Add to cart";
-    btn.addEventListener("click",function(){
-       addItems(elem);
-    })
-
-    div.append(image,price, btn);
-    append_div.append(div);
-    
-
-   })
-   
- }
-
-
- function addItems(elem){
-     
-     cartData.push(elem);
-     console.log(cartData);
-     localStorage.setItem('cartData', JSON.stringify(cartData));
- }
+	console.log("cartData:", cartData);
+	localStorage.setItem("cartData", JSON.stringify(cartData));
+}
